e11253422c82a7ba215e8200c6eafb35963c2c36
Esha Agarwal <esha.agarwal0212@gmail.com>
2025-03-12 22:25:57 +0100

    adds method to fetch blogs by creation date range

--- src/main/java/org/gisma/blogverse/services/repository/BlogRepository.java
+++ src/main/java/org/gisma/blogverse/services/repository/BlogRepository.java
@@ -3,6 +3,8 @@ package org.gisma.blogverse.services.repository;
 import org.gisma.blogverse.models.Blog;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BlogRepository extends MongoRepository<Blog, String> {
@@ -12,4 +14,7 @@ public interface BlogRepository extends MongoRepository<Blog, String> {
 
     // Fetch blogs by category
     List<Blog> findByCategory(String category);
+
+    // Fetch blogs by createdAt between the given start and end LocalDateTime
+    List<Blog> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
