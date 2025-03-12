0faf3726974130687706cf9cc8d35942b91e090f
Esha Agarwal <esha.agarwal0212@gmail.com>
2025-03-12 23:20:55 +0100

    refactors Comment model to remove unnecessary fields

--- src/main/java/org/gisma/blogverse/models/Comment.java
+++ src/main/java/org/gisma/blogverse/models/Comment.java
@@ -1,42 +1,29 @@
 package org.gisma.blogverse.models;
 
-import org.springframework.data.annotation.Id;
-
-import java.time.LocalDateTime;
+import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Objects;
 
+@Document(collection = "comments")
 public class Comment {
 
-    @Id
-    private String id; // Unique ID for the comment
     private String author;
     private String text;
-    private LocalDateTime createdAt = LocalDateTime.now();
 
     // Constructors
     public Comment(String author, String text) {
         this.author = author;
         this.text = text;
-        this.createdAt = LocalDateTime.now();
     }
 
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Comment comment)) return false;
-        return Objects.equals(getId(), comment.getId()) && Objects.equals(getAuthor(), comment.getAuthor()) && Objects.equals(getText(), comment.getText()) && Objects.equals(getCreatedAt(), comment.getCreatedAt());
+        return Objects.equals(getAuthor(), comment.getAuthor()) && Objects.equals(getText(), comment.getText());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getAuthor(), getText(), getCreatedAt());
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
+        return Objects.hash(getAuthor(), getText());
     }
 
     public String getAuthor() {
@@ -53,13 +40,5 @@ public class Comment {
 
     public void setText(String text) {
         this.text = text;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
     }
 }
