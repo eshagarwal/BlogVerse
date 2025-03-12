2a7ffa0d999512409fdc560dce4fadaa1b5f6773
Esha Agarwal <esha.agarwal0212@gmail.com>
2025-03-12 21:26:13 +0100

    adds BlogService class for managing blog operations

---
+++ src/main/java/org/gisma/blogverse/services/BlogService.java
@@ -0,0 +1,53 @@
+package org.gisma.blogverse.services;
+
+import org.gisma.blogverse.models.Blog;
+import org.gisma.blogverse.services.repository.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BlogService {
+
+    @Autowired
+    private BlogRepository blogRepository;
+
+    public Blog createBlog(Blog blog) {
+        return blogRepository.save(blog);
+    }
+
+    public List<Blog> getAllBlogs() {
+        return blogRepository.findAll();
+    }
+
+    public Optional<Blog> getBlogById(String id) {
+        return blogRepository.findById(id);
+    }
+
+    public List<Blog> getBlogsByCategory(String category) {
+        return blogRepository.findByCategory(category);
+    }
+
+    public Blog updateBlog(String id, Blog blogDetails) {
+        Optional<Blog> existingBlog = blogRepository.findById(id);
+        if (existingBlog.isPresent()) {
+            Blog blog = existingBlog.get();
+            blog.setTitle(blogDetails.getTitle());
+            blog.setContent(blogDetails.getContent());
+            blog.setAuthor(blogDetails.getAuthor());
+            blog.setCategory(blogDetails.getCategory());
+            return blogRepository.save(blog);
+        }
+        return null;
+    }
+
+    public boolean deleteBlogById(String id) {
+        Optional<Blog> existingBlog = blogRepository.findById(id);
+        if (existingBlog.isPresent()) {
+            blogRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
