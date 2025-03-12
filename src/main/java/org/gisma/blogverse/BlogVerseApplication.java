ce09e1b92c54e1ebe72bff153a79a49dc2dfed97
Esha Agarwal <esha.agarwal0212@gmail.com>
2025-03-12 21:26:33 +0100

    enables MongoDB auditing for automatic date fields

--- src/main/java/org/gisma/blogverse/BlogVerseApplication.java
+++ src/main/java/org/gisma/blogverse/BlogVerseApplication.java
@@ -2,11 +2,14 @@ package org.gisma.blogverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing // Enables automatic date fields
 public class BlogVerseApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BlogVerseApplication.class, args);
     }
 
