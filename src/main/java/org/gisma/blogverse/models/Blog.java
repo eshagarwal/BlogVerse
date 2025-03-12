b5615db31473c71a5c60f00bd17377e9fec99ed6
Esha Agarwal <esha.agarwal0212@gmail.com>
2025-03-12 22:58:51 +0100

    updates Blog model to use Comment objects for comments and adds equals/hashCode methods

--- src/main/java/org/gisma/blogverse/models/Blog.java
+++ src/main/java/org/gisma/blogverse/models/Blog.java
@@ -4,7 +4,9 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Document(collection = "blogs") // Maps this model to the "blogs" collection in MongoDB
 public class Blog {
@@ -18,7 +20,8 @@ public class Blog {
 
     @CreatedDate
     private LocalDateTime createdAt;
-    private List<String> comments;
+//    private List<String> comments;
+    private List<Comment> comments = new ArrayList<>();
 
     // Constructors
     public Blog() {}
@@ -28,6 +31,17 @@ public class Blog {
         this.content = content;
         this.author = author;
         this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Blog blog)) return false;
+        return Objects.equals(getId(), blog.getId()) && Objects.equals(getTitle(), blog.getTitle()) && Objects.equals(getContent(), blog.getContent()) && Objects.equals(getAuthor(), blog.getAuthor()) && Objects.equals(getCategory(), blog.getCategory()) && Objects.equals(getCreatedAt(), blog.getCreatedAt()) && Objects.equals(getComments(), blog.getComments());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getContent(), getAuthor(), getCategory(), getCreatedAt(), getComments());
     }
 
     // Getters and Setters
@@ -49,6 +63,6 @@ public class Blog {
     public LocalDateTime getCreatedAt() { return createdAt; }
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
 
-    public List<String> getComments() { return comments; }
-    public void setComments(List<String> comments) { this.comments = comments; }
+    public List<Comment> getComments() { return comments; }
+    public void setComments(List<Comment> comments) { this.comments = comments; }
 }
