8ed1b800417b709d998c5b6f348308bd0996c2b2
Esha Agarwal <esha.agarwal0212@gmail.com>
2025-03-12 23:37:41 +0100

    adds method to retrieve the top 5 most commented blogs

--- src/main/java/org/gisma/blogverse/services/repository/BlogRepository.java
+++ src/main/java/org/gisma/blogverse/services/repository/BlogRepository.java
@@ -3,18 +3,29 @@ package org.gisma.blogverse.services.repository;
 import org.gisma.blogverse.models.Blog;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Aggregation;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BlogRepository extends MongoRepository<Blog, String> {
-    // Fetch all available categories
+    // Get all available categories
     @Query(value = "{}", fields = "{ 'category' : 1 }")  // fetch the category field only
     List<Blog> findCategoryBy();
 
-    // Fetch blogs by category
+    // Get blogs by category
     List<Blog> findByCategory(String category);
 
-    // Fetch blogs by createdAt between the given start and end LocalDateTime
+    // Get blogs by createdAt between the given start and end LocalDateTime
     List<Blog> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    // Get the most commented blogs
+    @Aggregation(pipeline = {
+            "{ $addFields: { comments: { $ifNull: ['$comments', []] } } }",
+            "{ $project: { title: 1, category: 1, author: 1, createdAt: 1, comments: 1, commentCount: { $size: '$comments' } } }",
+            "{ $sort: { commentCount: -1 } }",
+            "{ $limit: 5 }"
+    })
+    List<Blog> findTop5ByMostComments();
+
 }
